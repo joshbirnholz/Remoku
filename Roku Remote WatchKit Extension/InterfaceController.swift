1939bd6afcd9ab7b9dfee189bf554136d3b2b083
Josh Birnholz <josh@birnholz.com>
2018-10-07 21:56:12 -0400

    Initial Commit

---
+++ Roku Remote WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Roku Remote WatchKit Extension
+//
+//  Created by Josh Birnholz on 10/7/18.
+//  Copyright © 2018 Josh Birnholz. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Roku Remote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Roku Remote
+//
+//  Created by Josh Birnholz on 10/7/18.
+//  Copyright © 2018 Josh Birnholz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
